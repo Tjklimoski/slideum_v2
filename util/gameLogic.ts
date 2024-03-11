c13b4b263ab0b85497743ad404edff82bb964745
TJ Klimoski <tjklimoski@gmail.com>
2024-03-11 15:37:19 -0400

    Setup gameLogic file in util directory

---
+++ util/gameLogic.ts
@@ -0,0 +1,13 @@
+// global variable that saves the correct board
+
+// funciton that randomizes board, validates it, and returns the starting board
+
+// function that validates a board passed in --> returns a new baord with status' set
+
+// VALIDATION HELPER FUNCS
+
+// function that gets all words on passed in board
+
+// checks for close (has a matching word on the board)
+
+// checks for correct (has a matching word in the write location)
